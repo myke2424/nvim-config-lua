ea8bf889d052490a667fc4ef6b6a1adf8bcf2a46
myke2424 <myke2424@gmail.com>
2022-08-12 19:49:40 -0400

    Nvim config

---
+++ lua/cfg/packer.lua
@@ -0,0 +1,17 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/nvim-lsp-installer'
+  use 'hrsh7th/nvim-cmp'
+
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+end)
---
+++ lua/cfg/set.lua
@@ -0,0 +1,24 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map("n", "<Space>t", ":Telescope find_files<CR>")
+map("n", "<Space>f", ":Telescope live_grep<CR>")
